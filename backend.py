d400d2326efd93cfe05ae07769a4bd4abccd2cb6
Ronald Adomako <ronaldadomako@icloud.com>
2020-10-24 15:06:11 -0400

    Drop table and added; primary key functions for update and delete WIP

--- backend.py
+++ backend.py
@@ -5,16 +5,20 @@ import mysql.connector
 def initiateTable():
     conn = mysql.connector.connect(database="mydb", user ="root", password = "password", host="localhost")
     cur = conn.cursor()
-    cur.execute('''
-        CREATE TABLE IF NOT EXISTS quantact1 (id int NOT NULL AUTO_INCREMENT, first text, last text, number text, email text, PRIMARY KEY (id));
-    ''')
+    #query0 = '''DROP DATABASE IF EXISTS Example3NF;'''
+    query1 = '''DROP TABLE IF EXISTS quantact;'''
+    query2 = '''
+        CREATE TABLE IF NOT EXISTS quantact (id INTEGER PRIMARY KEY AUTO_INCREMENT, first text, last text, number text, email text);
+    '''
+    cur.execute(query1)
+    cur.execute(query2)
     conn.commit()
     conn.close()
     
 def search(first,last,number, email):
     conn = mysql.connector.connect(database="mydb", user ="root", password = "password", host="localhost")
     cur = conn.cursor()
-    query = "SELECT * FROM quantact1 WHERE first='"+str(first)+"' OR last='"+str(last)+"' OR number='"+str(number)+"' OR email='"+str(email)+"'"
+    query = "SELECT * FROM quantact WHERE first='"+str(first)+"' OR last='"+str(last)+"' OR number='"+str(number)+"' OR email='"+str(email)+"';"
     cur.execute(query)
     rows = cur.fetchall()
     conn.close()
@@ -23,7 +27,7 @@ def search(first,last,number, email):
 def insert(first,last,number, email):
     conn = mysql.connector.connect(database="mydb", user ="root", password = "password", host="localhost")
     cur = conn.cursor()
-    query = "INSERT INTO quantact1 (first,last,number,email) VALUES ('" + first + "', '" + last + "', '" + str(number) + "', '" + email + "')"
+    query = "INSERT INTO quantact (first,last,number,email) VALUES ('" + first + "', '" + last + "', '" + str(number) + "', '" + email + "');"
     #query.format()
     #query = f'INSERT INTO ({first},{last},{number},{email}) VALUES (first, last, number, email)'
     #query.format( {first:'first'}, {last:'last'}, {number:'number'}, {email:'email'})
@@ -37,27 +41,31 @@ def view():
     conn = mysql.connector.connect(database="mydb", user ="root", password = "password", host="localhost")
     cur = conn.cursor()
     cur.execute('''
-        SELECT * FROM quantact1
+        SELECT * FROM quantact;
     ''')
     rows = cur.fetchall()
     conn.close()
     return rows
     
-    
 def delete(variable_id):
     conn = mysql.connector.connect(database="mydb", user ="root", password = "password", host="localhost")
     cur = conn.cursor()
-    cur.execute(
-        '''DELETE FROM quantact1 WHERE id=?''', (variable_id)
-    )
-    rows = cur.fetchall()
+    query = "DELETE FROM quantact WHERE id = "+str(variable_id)+";"
+    cur.execute(query)
+    #rows = cur.fetchall()
     conn.close()
+    #print('Your are deleting contact: ', rows)
+    return
     
 def update(variable_id,first,last,number, email):
     conn = mysql.connector.connect(database="mydb", user ="root", password = "password", host="localhost")
     cur = conn.cursor()
-    query = "UPDATE quantact1 SET first='"+first+"',last='"+last+"', number='"+number+"', email='"+email+"', where id='"+variable_id+"'"
+    query = "UPDATE quantact SET first='"+first+"',last='"+last+"', number='"+number+"', email='"+email+"' where id = "+str(variable_id)+";"
     cur.execute(query)
     #rows = cur.fetchone()
     conn.close()
-    return search(first,last,number, email)
+    #return search(first,last,number, email)
+    return
+
+def reset():
+    return
