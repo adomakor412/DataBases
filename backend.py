c511a5c82606abdfb80f335df5e42d5fa973edf3
Ronald Adomako <ronaldadomako@icloud.com>
2020-10-24 23:41:04 -0400

    CRUD commit

--- backend.py
+++ backend.py
@@ -8,7 +8,7 @@ def initiateTable():
     #query0 = '''DROP DATABASE IF EXISTS Example3NF;'''
     query1 = '''DROP TABLE IF EXISTS quantact;'''
     query2 = '''
-        CREATE TABLE IF NOT EXISTS quantact (id INTEGER PRIMARY KEY AUTO_INCREMENT, first text, last text, number text, email text);
+        CREATE TABLE IF NOT EXISTS quantact (prime_id INTEGER KEY AUTO_INCREMENT, first text, last text, number text, email text);
     '''
     cur.execute(query1)
     cur.execute(query2)
@@ -48,24 +48,28 @@ def view():
     return rows
     
 def delete(variable_id):
-    conn = mysql.connector.connect(database="mydb", user ="root", password = "password", host="localhost")
+    conn = mysql.connector.connect(database="mydb", user ="root", password = "password", host="localhost",buffered=True)
     cur = conn.cursor()
-    query = "DELETE FROM quantact WHERE id = "+str(variable_id)+";"
-    cur.execute(query)
+    query1= '''SELECT * FROM quantact;'''
+    query2 = "DELETE FROM quantact WHERE prime_id ="+str(variable_id)
+    cur.execute(query1)
+    cur.execute(query2)
     #rows = cur.fetchall()
+    conn.commit()
     conn.close()
     #print('Your are deleting contact: ', rows)
-    return
+    return 
     
 def update(variable_id,first,last,number, email):
-    conn = mysql.connector.connect(database="mydb", user ="root", password = "password", host="localhost")
+    conn = mysql.connector.connect(database="mydb", user ="root", password = "password", host="localhost",buffered=True)
     cur = conn.cursor()
-    query = "UPDATE quantact SET first='"+first+"',last='"+last+"', number='"+number+"', email='"+email+"' where id = "+str(variable_id)+";"
+    query = "UPDATE quantact SET first='"+first+"',last='"+last+"', number='"+number+"', email='"+email+"' where prime_id = "+str(variable_id)+";"
     cur.execute(query)
     #rows = cur.fetchone()
+    conn.commit()
     conn.close()
     #return search(first,last,number, email)
     return
 
 def reset():
-    return
+    return 
