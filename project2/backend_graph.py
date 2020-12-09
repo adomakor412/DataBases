a36505bc4907001d8dfbbcba228176ba6edba515
Ronald Adomako <ronaldadomako@icloud.com>
2020-12-09 15:45:05 -0500

    question 1 part 1 view results

--- project2/backend_graph.py
+++ project2/backend_graph.py
@@ -1,40 +1,47 @@
 import mysql.connector
 
 
-def initiateTable(source,target):
+def initiateTable():
     conn = mysql.connector.connect(database="mydb", user ="root", password = "password", host="localhost")
     cur = conn.cursor()
-    query = f'''
-        CREATE TABLE DirectedEdges(f{source} INT, f{target} INT, PRIMARY KEY (f{source}, f{target}));
-
-        LOAD DATA LOCAL INFILE 'question1/graph.csv'
-        INTO TABLE DirectedEdges
+    query1 = '''DROP TABLE IF EXISTS DirectedEdges;'''
+    query2 = '''
+        CREATE TABLE DirectedEdges(source INT, target INT, PRIMARY KEY (source, target));
+        '''
+    query3 = ''' 
+        SET GLOBAL local_infile=1;
+        SHOW VARIABLES LIKE "question1/graph.csv";
+        LOAD DATA INFILE 'question1/graph.csv' INTO TABLE DirectedEdges
         FIELDS TERMINATED BY ','
         ENCLOSED BY '"'
-        LINES TERMINATED BY '\n'
-        GNORE 1 ROWS;
+        LINES TERMINATED BY ''
+        IGNORE 1 ROWS;
         '''
-    cur.execute(query)
+    cur.execute(query1) 
+    cur.execute(query2)
     conn.commit()
+    cur.execute(query3) #You cannot commit procedures, not a CRUD operation
+    #conn.commit()
     conn.close()
     return
     
 def checkConnected(source,target,length):
     conn = mysql.connector.connect(database="mydb", user ="root", password = "password", host="localhost")
     cur = conn.cursor()
-    query = f'''
+    inputs = (source,target,length)
+    query = '''
         DROP PROCEDURE IF EXISTS checkConnected;
         DELIMITER $$
-        CREATE PROCEDURE checkConnected(IN {source} INT, IN {target} INT, IN {length} INT, OUT connected CHAR(20))
+        CREATE PROCEDURE checkConnected(IN source INT, IN target INT, IN length INT, OUT connected CHAR(20))
         BEGIN
             declare len_count INT;
             SELECT COUNT(*)
             FROM
             (SELECT *
             FROM DirectedEdges 
-            WHERE DirectedEdges.{source} between {source} and {target}
-            and directededges.{target} = {target} 
-            LIMIT {length}) as A
+            WHERE DirectedEdges.source between source and target
+            and directededges.target = target
+            LIMIT length) as A
             INTO len_count;
             IF len_count - 1 < length and len_count > 0
             THEN
@@ -44,10 +51,10 @@ def checkConnected(source,target,length):
             END IF;
         END $$
         DELIMITER ;
-        CALL checkConnected(3, 12, 3, @connected);
+        CALL checkConnected(%s, %s, %s, @connected);
         SELECT @connected;
     '''
-    connected = cur.execute(query)
+    connected = cur.execute(query, inputs)
     conn.close()
     return connected
 
